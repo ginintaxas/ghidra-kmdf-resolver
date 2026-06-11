d2d5e7214a34d9ccfb9a3d558c81492b4237748a
ginintaxas <eigliutk@gmail.com>
2026-06-11 20:51:06 +0300

    added functionality to find the wdffunc address

---
+++ KmdfFunctionResolve.py
@@ -0,0 +1,77 @@
+#TODO write a description for this script
+#@ginintaxas
+#@category KMDF
+try:
+    _ = currentProgram
+except:
+    from ghidra.ghidra_builtins import *
+
+from ghidra import *
+from ghidra.app.decompiler import DecompInterface
+from ghidra.util.task import ConsoleTaskMonitor
+from ghidra.program.model.listing import Function
+from ghidra.program.model.symbol import ThunkReference
+from ghidra.program.model.pcode import PcodeOp
+from ghidra.program.model.data import DataUtilities
+
+listing = currentProgram.getListing()
+
+#return address of the call to wdf version bind
+def get_vb_ref():
+    symbolTable = currentProgram.getSymbolTable()
+    ref_manager = currentProgram.getReferenceManager()
+
+    function_name = "WdfVersionBind"
+    symbols = symbolTable.getSymbols(function_name)
+    if symbols.hasNext():
+        sym = symbols.next()
+    if(len(list(symbols)) > 1):
+        raise ValueError(f"There can only be one of the external symbol {function_name}")
+    for ref in sym.getReferences():
+        if ref.getReferenceType().isCall() or isinstance(ref, ThunkReference):
+            thunk_addr = ref.getFromAddress()
+    for ref in ref_manager.getReferencesTo(thunk_addr):
+        if ref.getReferenceType().isCall():
+            return ref
+    return None
+
+def find_wdf_bind_info_addr(vb_addr):
+    print(vb_addr)
+    addr = None
+    cur_instr = listing.getInstructionAt(vb_addr)
+    for instr_count in range(30):
+        mnemonic = cur_instr.getMnemonicString()
+        if mnemonic in ["MOV", "LEA"]:
+            dest_reg = cur_instr.getRegister(0)
+            if dest_reg.getName().upper() == "R8":
+                break
+        cur_instr = cur_instr.getPrevious()
+    if instr_count == 29:
+        return None
+    wdf_bind_addr = cur_instr.getPrimaryReference(1).getToAddress()
+    return wdf_bind_addr
+    
+def extract_wdffunc_ptr(bind_info_addr):
+    dtm = currentProgram.getDataTypeManager()
+    all_dt = dtm.getAllDataTypes()
+    for dt in all_dt:
+        if dt.getName() == "WDF_BIND_INFO":
+            data = listing.getDataAt(bind_info_addr)
+            if not data.isDefined():
+                data = listing.createData(bind_info_addr, dt)
+            #wdf_bind_info offset for func table is 0x20
+            func_tbl_ptr_data = data.getComponentContaining(0x20)
+            if func_tbl_ptr_data.isPointer():
+                return func_tbl_ptr_data.getValue()
+    return None
+
+def get_wdffunc_addr():
+    vb_call_addr = get_vb_ref().getFromAddress()
+    bind_info = find_wdf_bind_info_addr(vb_call_addr)
+    func_table_addr = extract_wdffunc_ptr(bind_info)
+    return func_table_addr
+
+if __name__ == "__main__":
+    tbl_addr = get_wdffunc_addr
+
+
