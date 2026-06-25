ce4304c49f6a2af29c87f7217d866a6e8a64781a
ginintaxas <eigliutk@gmail.com>
2026-06-25 13:30:26 +0300

    fixed data type not found

--- kmdf_function_resolve.py
+++ kmdf_function_resolve.py
@@ -12,6 +12,7 @@ from ghidra.program.model.listing import Function
 from ghidra.program.model.symbol import ThunkReference
 from ghidra.program.model.pcode import PcodeOp
 from ghidra.program.model.data import DataUtilities, DataTypeConflictHandler, PointerDataType
+from ghidra.app.services import DataTypeManagerService
 
 listing = currentProgram.getListing()
 dtm = currentProgram.getDataTypeManager()
@@ -51,21 +52,23 @@ def find_wdf_bind_info_addr(vb_addr):
     
 def extract_wdffunc_ptr(bind_info_addr):
     print(f"found wdf bind info address: {bind_info_addr}")
-    all_dt = dtm.getAllDataTypes()
-    for dt in all_dt:
-        if dt.getName() == "WDF_BIND_INFO":
-            data = listing.getDataAt(bind_info_addr)
-            current_type_name = data.getDataType().getName()
-            if not "WDF_BIND_INFO" in current_type_name:
-                listing.clearCodeUnits(bind_info_addr, bind_info_addr.add(dt.getLength() - 1), True)
-                data = listing.createData(bind_info_addr, dt)
+    service = state.getTool().getService(DataTypeManagerService)
+    all_managers = service.getDataTypeManagers()
+    for mgr in all_managers:
+        for dt in mgr.getAllDataTypes():
+            if dt.getName() == "WDF_BIND_INFO":
+                data = listing.getDataAt(bind_info_addr)
+                current_type_name = data.getDataType().getName()
+                if not "WDF_BIND_INFO" in current_type_name:
+                    listing.clearCodeUnits(bind_info_addr, bind_info_addr.add(dt.getLength() - 1), True)
+                    data = listing.createData(bind_info_addr, dt)
             
-            #wdf_bind_info offset for func table is 0x20
-            func_tbl_ptr_data = data.getComponentContaining(0x20)
-            if func_tbl_ptr_data.isPointer():
-                return func_tbl_ptr_data.getValue()
-            else:
-                raise ValueError("Couldnt extract the function table pointer value")
+                #wdf_bind_info offset for func table ptr is 0x20
+                func_tbl_ptr_data = data.getComponentContaining(0x20)
+                if func_tbl_ptr_data.isPointer():
+                    return func_tbl_ptr_data.getValue()
+                else:
+                    raise ValueError("Couldnt extract the function table pointer value")
     raise ValueError("WDF_BIND_INFO not found, did you import the correct data types?")
 
 def get_wdffunc_addr():
@@ -75,16 +78,18 @@ def get_wdffunc_addr():
     return func_table_addr
 
 def resolve_func_addrs(tableptr_address):
-    all_dt = dtm.getAllDataTypes()
-    for dt in all_dt:
-        if dt.getName() == "WDFFUNCTIONS":
-            data = listing.getDataAt(tableptr_address)
-            current_type_name = data.getDataType().getName()
-            if not "WDFFUNCTIONS *" in current_type_name:
-                listing.clearCodeUnits(tableptr_address, tableptr_address.add(7), True)
-                func_ptr_type = PointerDataType(dt, dtm)
-                listing.createData(tableptr_address, func_ptr_type)
-            return
+    service = state.getTool().getService(DataTypeManagerService)
+    all_managers = service.getDataTypeManagers()
+    for mgr in all_managers:
+        for dt in mgr.getAllDataTypes():
+            if dt.getName() == "WDFFUNCTIONS":
+                data = listing.getDataAt(tableptr_address)
+                current_type_name = data.getDataType().getName()
+                if not "WDFFUNCTIONS *" in current_type_name:
+                    listing.clearCodeUnits(tableptr_address, tableptr_address.add(7), True)
+                    func_ptr_type = PointerDataType(dt, dtm)
+                    listing.createData(tableptr_address, func_ptr_type)
+                return
     raise ValueError("WDFFUNCTIONS not found, did you import the correct data types?")
 
 if __name__ == "__main__":
